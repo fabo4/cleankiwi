ee4c23893a4dc2dc76004fdfb67f1aeebfbf7aa1
Ondrej Fabian <ondrej.fabian@cleverlance.com>
2018-11-26 21:51:04 +0100

    Set root SplashViewController resolved from InstanceProvider

---
+++ CleanKiwiCore/CleanKiwiCore.h
@@ -0,0 +1,19 @@
+//
+//  CleanKiwiCore.h
+//  CleanKiwiCore
+//
+//  Created by Ondrej Fabian on 26/11/2018.
+//  Copyright © 2018 Kiwi.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CleanKiwiCore.
+FOUNDATION_EXPORT double CleanKiwiCoreVersionNumber;
+
+//! Project version string for CleanKiwiCore.
+FOUNDATION_EXPORT const unsigned char CleanKiwiCoreVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CleanKiwiCore/PublicHeader.h>
+
+
---
+++ Podfile
@@ -0,0 +1,13 @@
+platform :ios, '11.0'
+
+target 'CleanKiwi' do
+
+  use_frameworks!
+
+  target 'CleanKiwiTests' do
+    inherit! :search_paths
+  
+  end
+
+end
+
